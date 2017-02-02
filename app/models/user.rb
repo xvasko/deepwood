b1898b721f5062452b899260af13cd75fa2803cd
Matej Vasko <matej.vasko@outlook.com>
2017-02-02 16:33:03 +0100

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password # vytvori virtualne atributy :password a :password_confirmation, virtualne = neexistuju v DB
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
