7372d28e15a63dfac1f0eee6ac106c75c3a8b5c0
Matej Vasko <matej.vasko@outlook.com>
2017-02-06 18:19:09 +0100

    checkpoint

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "Post created!"
       redirect_to root_url
     else
       @feed_items = current_user.feed.paginate(page: params[:page]) # mozno sa bude dat vyriesit inak!!!!!
@@ -15,7 +15,7 @@ class MicropostsController < ApplicationController
 
   def destroy
     @micropost.destroy
-    flash[:success] = "Micropost deleted"
+    flash[:success] = "Post deleted!"
     redirect_to request.referrer || root_url # request.referrer je previous URL, kedze deletujeme z root ale aj z profilu aby sme sa vratili s5 (root_url kvoli testom)
   end
 
@@ -25,8 +25,8 @@ class MicropostsController < ApplicationController
       params.require(:micropost).permit(:content, :picture)
     end
 
-  def correct_user
-    @micropost = current_user.microposts.find_by(id: params[:id])
-    redirect_to root_url if @micropost.nil?
-  end
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
