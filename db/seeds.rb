6fbe1afdaf4721a932c43fc83176d1ae51043769
Matej Vasko <matej.vasko@outlook.com>
2017-02-06 14:35:56 +0100

    User bars

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Matej Vasko",
              activated: true,
              activated_at: Time.zone.now)
 
-10.times do |n|
+99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@email.com"
   password = "password"
